a649ba23b6083b552ca01062a50a78cb32583c69
Joachim Feltkamp <j.feltkamp@gmail.com>
2023-01-05 18:50:45 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RulesFinder'
+copyright = '2022, Joachim Feltkamp'
+author = 'Joachim Feltkamp'
 
 release = '0.1'
 version = '0.1.0'
